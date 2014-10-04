823aad90fdc80da7d728b7839d1e98005bf2a4fa
Michiel Sikkes <michiel@firmhouse.com>
2014-10-04 23:47:50 +0200

    Added test that verifies the app database is created

--- test/integration/full-stack/serverspec/apps_spec.rb
+++ test/integration/full-stack/serverspec/apps_spec.rb
@@ -3,3 +3,8 @@ require 'spec_helper'
 describe file("/u/apps/intercity_sample_app") do
   it { should be_directory }
 end
+
+describe command('mysql -u intercity --password=r4nd0m -e "show databases;"') do
+  its (:stdout) { should match /intercity_sample_app/ }
+  its (:exit_status) { should eq 0 }
+end
